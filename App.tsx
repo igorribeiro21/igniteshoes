efc169028d44637becbe8ea501e13545ddb2a4ca
igor.ribeiro <igorapalvesribeiro@gmail.com>
2023-11-14 22:03:00 -0300

    push-notification - feat: added new tag user_email

--- App.tsx
+++ App.tsx
@@ -8,12 +8,16 @@ import { Routes } from './src/routes';
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
+
 import { CartContextProvider } from './src/contexts/CartContext';
 
 OneSignal.initialize('9d081469-1f39-4d53-a13a-afeb611994d4');
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate('igorapalvesribeiro@gmail.com');
 
   return (
     <NativeBaseProvider theme={THEME}>
