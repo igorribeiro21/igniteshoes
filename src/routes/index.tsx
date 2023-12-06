7130a8ea1d26a756977551e0f8162b54e9c4af8e
igor.ribeiro <igorapalvesribeiro@gmail.com>
2023-12-05 22:04:31 -0300

    push-notification - feat: finalized push notification

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -28,7 +28,7 @@ export function Routes() {
       {
         notification?.title &&
         <Notification
-          title={notification?.title}
+          data={notification}
           onClose={(() => { setNotification(undefined) })}
         />}
     </NavigationContainer>
